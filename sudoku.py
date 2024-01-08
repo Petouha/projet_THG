a8454c76e115995a65b21e4b0d2b4af38f1645c8
petouha <rachidgames2.rg@gmail.com>
2024-01-08 22:38:01 +0100

    commentaires

--- sudoku.py
+++ sudoku.py
@@ -5,26 +5,28 @@ from src.backtracking_clean import *
 def create_sudoku_empty(taille):
     
     liste =[]
+    #boucler sur le sudoku en mettant les relations dans une liste de listes
     for i in range(taille*taille):
-        
         for j in range(taille*taille):
+            #pour chaque sommet, relier avec la ligne
             sublist = []
             for k in range(taille*taille):
                 if k!=j:
                     sublist.append(((i)*taille*taille)+k)
-            
+            #relier avec la colonne
             for k in range(taille*taille):  
                 if k!=i:
                     sublist.append((j)+k*taille*taille)
-            
+            #relier avec le carré correspondant
             for k in range((i // taille)*taille,((i // taille)*taille+taille)):
                 for l in range((j // taille)*taille,((j // taille)*taille+taille)):
                     if k!=i:
                         sublist.append(k*taille*taille+l)
-            
+            #enlever les doublons
             sublist = list(set(sublist))
             liste.append(sublist)
             
+    #retourner le graph
     dico = {}
     for i, sublist in enumerate(liste):
         dico[i] = sublist
@@ -36,6 +38,7 @@ def solve_sudoku(graph):
     if not is_valid_sudoku(graph):
         return None
     
+    #mettre les cases entre 0 et 8
     taille = np.shape(graph)[0]
     for i in range(taille):
         for j in range(taille):
@@ -47,7 +50,7 @@ def solve_sudoku(graph):
 
     if solved is None:
         return None
-    
+    #remettre les cases entre 1 et 9
     for i in range(np.shape(solved)[0]):
         for j in range(np.shape(solved)[0]):
             solved[i][j]=solved[i][j]+1
